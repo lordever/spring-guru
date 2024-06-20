914bedf531cbaa72234c36c12af9974e4eb38656
lordever <lordever@mail.ru>
2024-06-20 14:57:21 +0500

    Project init

---
+++ src/main/kotlin/com/zoom/webapp/springguruwebapp/SpringGuruWebappApplication.kt
@@ -0,0 +1,11 @@
+package com.zoom.webapp.springguruwebapp
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringGuruWebappApplication
+
+fun main(args: Array<String>) {
+    runApplication<SpringGuruWebappApplication>(*args)
+}
---
+++ src/test/kotlin/com/zoom/webapp/springguruwebapp/SpringGuruSec1WebappApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.zoom.webapp.springguruwebapp
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringGuruSec1WebappApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
