18abff5e0a139f8a52a28b68a6dc38fd858b9e28
alzh0817 <aleksandr.zheleznov@netcracker.com>
2024-06-27 17:56:46 +0500

    Lesson 3.1

---
+++ spring-rest-mvc/src/main/kotlin/com/spring_guru/spring_rest_mvc/SpringRestMvcApplication.kt
@@ -0,0 +1,11 @@
+package com.spring_guru.spring_rest_mvc
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringRestMvcApplication
+
+fun main(args: Array<String>) {
+	runApplication<SpringRestMvcApplication>(*args)
+}
