51c225c1b5db8ee5deafd1ac0930ed42727325cf
alzh0817 <aleksandr.zheleznov@netcracker.com>
2024-06-28 15:20:12 +0500

    Lesson 4.2

--- kotlin-spring-rest-mvc/src/main/kotlin/com/kotlin_spring_rest_mvc/kotlin_spring_rest_mvc/controllers/BeerController.kt
+++ kotlin-spring-rest-mvc/src/main/kotlin/com/kotlin_spring_rest_mvc/kotlin_spring_rest_mvc/controllers/BeerController.kt
@@ -3,21 +3,26 @@ package com.kotlin_spring_rest_mvc.kotlin_spring_rest_mvc.controllers
 import com.kotlin_spring_rest_mvc.kotlin_spring_rest_mvc.models.Beer
 import com.kotlin_spring_rest_mvc.kotlin_spring_rest_mvc.services.BeerService
 import mu.KotlinLogging
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestMethod
 import org.springframework.web.bind.annotation.RestController
 import java.util.*
 
 @RestController
+@RequestMapping("/api/v1/beers")
 class BeerController(private val beerService: BeerService) {
 
     private val logger = KotlinLogging.logger {}
 
-    @RequestMapping("/api/v1/beers")
+    @GetMapping
     fun listBeers(): List<Beer> {
         return beerService.listBeer()
     }
 
-    fun getBeerById(id: UUID): Beer? {
+    @GetMapping("/{id}")
+    fun getBeerById(@PathVariable("id") id: UUID): Beer? {
         logger.debug { "Get beer by id $id" }
         return beerService.getBeerById(id)
     }
