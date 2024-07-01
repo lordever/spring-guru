3e328a0acc85571da5e334d18ad488ad747326bd
alzh0817 <aleksandr.zheleznov@netcracker.com>
2024-07-01 23:55:49 +0500

    Lesson 5.1

--- kotlin-spring-rest-mvc/src/test/kotlin/com/kotlin_spring_rest_mvc/kotlin_spring_rest_mvc/controllers/BeerControllerTest.kt
+++ kotlin-spring-rest-mvc/src/test/kotlin/com/kotlin_spring_rest_mvc/kotlin_spring_rest_mvc/controllers/BeerControllerTest.kt
@@ -1,17 +1,31 @@
 package com.kotlin_spring_rest_mvc.kotlin_spring_rest_mvc.controllers
 
+import com.kotlin_spring_rest_mvc.kotlin_spring_rest_mvc.services.BeerService
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
+import org.springframework.boot.test.mock.mockito.MockBean
+import org.springframework.http.MediaType
+import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
 import java.util.*
 
-@SpringBootTest
+@WebMvcTest(BeerController::class)
 class BeerControllerTest {
 
     @Autowired
-    lateinit var controller: BeerController
+    lateinit var mockMvc: MockMvc
+
+    @MockBean
+    lateinit var service: BeerService
+
     @Test
     fun getBeerById() {
-        println(controller.getBeerById(UUID.randomUUID()))
+        mockMvc.perform(
+            get("/api/v1/beers/" + UUID.randomUUID())
+                .accept(MediaType.APPLICATION_JSON)
+        )
+            .andExpect(status().isOk)
     }
 }
