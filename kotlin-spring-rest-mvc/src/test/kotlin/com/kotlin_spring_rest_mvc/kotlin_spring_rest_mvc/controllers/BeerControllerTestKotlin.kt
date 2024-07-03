65f1d628a22c872beb20d0a8090488f39deb7090
alzh0817 <aleksandr.zheleznov@netcracker.com>
2024-07-03 13:45:42 +0500

    Lesson 5.3 (1)

--- kotlin-spring-rest-mvc/src/test/kotlin/com/kotlin_spring_rest_mvc/kotlin_spring_rest_mvc/controllers/BeerControllerTestKotlin.kt
+++ kotlin-spring-rest-mvc/src/test/kotlin/com/kotlin_spring_rest_mvc/kotlin_spring_rest_mvc/controllers/BeerControllerTestKotlin.kt
@@ -2,37 +2,41 @@ package com.kotlin_spring_rest_mvc.kotlin_spring_rest_mvc.controllers
 
 import com.kotlin_spring_rest_mvc.kotlin_spring_rest_mvc.services.BeerService
 import com.kotlin_spring_rest_mvc.kotlin_spring_rest_mvc.services.BeerServiceImpl
-import io.mockk.every
-import io.mockk.mockk
+import org.hamcrest.CoreMatchers.equalTo
 import org.junit.jupiter.api.Test
+import org.mockito.BDDMockito.given
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
+import org.springframework.boot.test.mock.mockito.MockBean
 import org.springframework.http.MediaType
 import org.springframework.test.web.servlet.MockMvc
-import org.springframework.test.web.servlet.get
-import java.util.*
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.*
 
 @WebMvcTest(BeerController::class)
 class BeerControllerTestKotlin {
-    @Autowired
-    private lateinit var mockMvc: MockMvc
 
-    private val beerService: BeerService = mockk()
+    @Autowired
+    lateinit var mockMvc: MockMvc
+
+    @MockBean
+    lateinit var beerService: BeerService
+
     private val beerServiceImpl = BeerServiceImpl()
 
     @Test
     fun getBeerById() {
         val testBeer = beerServiceImpl.listBeer().first()
-        val beerId = UUID.randomUUID()
 
-        every { beerService.getBeerById(any()) } returns testBeer
+        given(beerService.getBeerById(testBeer.id)).willReturn(testBeer)
 
-        mockMvc.get("/api/v1/beers/$beerId") {
-            accept(MediaType.APPLICATION_JSON)
-        }.andExpect {
-            status { isOk() }
-            content { contentType(MediaType.APPLICATION_JSON) }
-        }
+        mockMvc.perform(
+            get("/api/v1/beers/${testBeer.id}")
+                .accept(MediaType.APPLICATION_JSON)
+        )
+            .andExpect(status().isOk)
+            .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+            .andExpect(jsonPath("$.id", equalTo(testBeer.id.toString())))
+            .andExpect(jsonPath("$.name", equalTo(testBeer.name)))
     }
-
 }
