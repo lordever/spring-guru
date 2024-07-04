c7eae9dfaeb8f64c49183b6599cf559246717fd1
lordever <lordever@mail.ru>
2024-07-04 23:28:32 +0500

    Lesson 5.7

--- kotlin-spring-rest-mvc/src/test/kotlin/com/kotlin_spring_rest_mvc/kotlin_spring_rest_mvc/controllers/BeerControllerTest.java
+++ kotlin-spring-rest-mvc/src/test/kotlin/com/kotlin_spring_rest_mvc/kotlin_spring_rest_mvc/controllers/BeerControllerTest.java
@@ -13,11 +13,13 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.util.UUID;
+
 import static org.hamcrest.core.Is.is;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.mockito.Mockito.verify;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @WebMvcTest(BeerController.class)
@@ -79,5 +81,18 @@ class BeerControllerTest {
                         .content(objectMapper.writeValueAsString(testBeer)))
                 .andExpect(status().isCreated())
                 .andExpect(header().exists("Location"));
+    }
+
+    @Test
+    void updateBeer() throws Exception {
+        Beer testBeer = beerServiceImpl.listBeer().getFirst();
+
+        mockMvc.perform(put("/api/v1/beers/" + testBeer.getId())
+                        .accept(MediaType.APPLICATION_JSON)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(testBeer)))
+                .andExpect(status().isNoContent());
+
+        verify(beerService).updateById(any(UUID.class), any(Beer.class));
     }
 }
