370b40640a35989341bb3f1f55f8cdb78f72b850
lordever <lordever@mail.ru>
2024-07-14 13:40:10 +0500

    Lesson 7.12

--- kotlin-spring-data-jpa/src/test/kotlin/com/kotlin_spring_data_jpa/kotlin_spring_data_jpa/controllers/BeerControllerIT.kt
+++ kotlin-spring-data-jpa/src/test/kotlin/com/kotlin_spring_data_jpa/kotlin_spring_data_jpa/controllers/BeerControllerIT.kt
@@ -3,11 +3,13 @@ package com.kotlin_spring_data_jpa.kotlin_spring_data_jpa.controllers
 import com.kotlin_spring_data_jpa.kotlin_spring_data_jpa.models.BeerDTO
 import com.kotlin_spring_data_jpa.kotlin_spring_data_jpa.repositories.BeerRepository
 import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Assertions.assertThrows
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.test.annotation.Rollback
 import org.springframework.transaction.annotation.Transactional
+import java.util.*
 
 @SpringBootTest
 class BeerControllerIT {
@@ -34,5 +36,21 @@ class BeerControllerIT {
         val dtos: List<BeerDTO> = beerController.listBeers()
 
         assertThat(dtos.size).isEqualTo(0)
+    }
+
+    @Test
+    fun testGetBeerById() {
+        val beer = beerRepository.findAll()[0]
+        val beerId = requireNotNull(beer.id) { "Beer ID cannot be null" }
+        val beerDTO = beerController.getBeerById(beerId)
+
+        assertThat(beerDTO).isNotNull
+    }
+
+    @Test
+    fun testGetBeerByIdNotFound() {
+        assertThrows(NotFoundException::class.java) {
+            beerController.getBeerById(UUID.randomUUID())
+        }
     }
 }
