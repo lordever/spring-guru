68709a35f1c9f5d78df8cc87a75de01ac4595df8
lordever <lordever@mail.ru>
2024-07-15 23:14:30 +0500

    Lesson 8.4

--- kotlin-spring-data-jpa/src/main/kotlin/com/kotlin_spring_data_jpa/kotlin_spring_data_jpa/controllers/CustomErrorController.kt
+++ kotlin-spring-data-jpa/src/main/kotlin/com/kotlin_spring_data_jpa/kotlin_spring_data_jpa/controllers/CustomErrorController.kt
@@ -9,9 +9,12 @@ import org.springframework.web.bind.annotation.ExceptionHandler
 @ControllerAdvice
 class CustomErrorController {
     @ExceptionHandler(MethodArgumentNotValidException::class)
-    fun handleBindErrors(exception: MethodArgumentNotValidException): ResponseEntity<MutableList<FieldError>> {
+    fun handleBindErrors(exception: MethodArgumentNotValidException): ResponseEntity<List<Map<String, String?>>> {
+        val errorList: List<Map<String, String?>> =
+            exception.fieldErrors.map { fieldError -> mapOf(fieldError.field to fieldError.defaultMessage) }
+
         return ResponseEntity
             .badRequest()
-            .body(exception.bindingResult.fieldErrors)
+            .body(errorList)
     }
 }
