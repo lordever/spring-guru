896a969d9a3d2ec828807966f824dc2d4e7216e5
lordever <lordever@mail.ru>
2024-08-22 18:37:58 +0500

    Lesson 16.1

---
+++ kotlin-spring-rest-template/src/main/kotlin/com/kotlin_spring_rest_template/kotlin_spring_rest_template/KotlinSpringRestTemplateApplication.kt
@@ -0,0 +1,11 @@
+package com.kotlin_spring_rest_template.kotlin_spring_rest_template
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotlinSpringRestTemplateApplication
+
+fun main(args: Array<String>) {
+	runApplication<KotlinSpringRestTemplateApplication>(*args)
+}
---
+++ kotlin-spring-rest-template/src/test/kotlin/com/kotlin_spring_rest_template/kotlin_spring_rest_template/KotlinSfgRestfulBreweryApplicationTests.kt
@@ -0,0 +1,11 @@
+package com.kotlin_spring_rest_template.kotlin_spring_rest_template
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinSfgRestfulBreweryApplicationTests {
+    @Test
+    fun contextLoads() {
+    }
+}
