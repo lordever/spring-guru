f844986387d9dd4c5ea95bb439a7207ffc32eec6
lordever <lordever@mail.ru>
2024-08-26 19:58:18 +0500

    Lesson 17.1

--- kotlin-spring-rest-template/src/test/kotlin/com/kotlin_spring_rest_template/kotlin_spring_rest_template/client/BeerClientImplTest.kt
+++ kotlin-spring-rest-template/src/test/kotlin/com/kotlin_spring_rest_template/kotlin_spring_rest_template/client/BeerClientImplTest.kt
@@ -1,6 +1,8 @@
 package com.kotlin_spring_rest_template.kotlin_spring_rest_template.client
 
+import com.fasterxml.jackson.databind.ObjectMapper
 import com.kotlin_spring_rest_template.kotlin_spring_rest_template.model.BeerDTO
+import com.kotlin_spring_rest_template.kotlin_spring_rest_template.model.BeerDTOPageImpl
 import com.kotlin_spring_rest_template.kotlin_spring_rest_template.model.BeerStyle
 import com.kotlin_spring_rest_template.kotlin_spring_rest_template.model.ListBeersFilter
 import jakarta.transaction.Transactional
@@ -8,22 +10,46 @@ import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.client.RestClientTest
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.http.HttpMethod
+import org.springframework.http.MediaType
+import org.springframework.test.web.client.MockRestServiceServer
+import org.springframework.test.web.client.match.MockRestRequestMatchers.method
+import org.springframework.test.web.client.match.MockRestRequestMatchers.requestTo
+import org.springframework.test.web.client.response.MockRestResponseCreators.withSuccess
 import org.springframework.web.client.HttpClientErrorException
 import java.math.BigDecimal
 import java.util.*
 
-@SpringBootTest
+@RestClientTest(BeerClientImpl::class)
 class BeerClientImplTest {
+
+    companion object {
+        const val URL = "http://locahost:8080"
+    }
 
     @Autowired
     lateinit var beerClient: BeerClientImpl
 
+    @Autowired
+    lateinit var server: MockRestServiceServer
+
+    @Autowired
+    lateinit var objectMapper: ObjectMapper
+
     @Test
     fun listBeers() {
+        val payload = objectMapper.writeValueAsString(getPage())
+
+        server.expect(method(HttpMethod.GET))
+            .andExpect(requestTo(URL + BeerClientImpl.GET_BEER_PATH))
+            .andRespond(withSuccess(payload, MediaType.APPLICATION_JSON))
+
         val filter = ListBeersFilter()
 
-        beerClient.listBeers(filter)
+        val beers = beerClient.listBeers(filter)
+        assertThat(beers?.size).isGreaterThan(0)
     }
 
     @Test
@@ -180,4 +206,16 @@ class BeerClientImplTest {
             beerClient.getBeerById(savedBeer?.id)
         }
     }
+
+    private fun getPage(): BeerDTOPageImpl =
+        BeerDTOPageImpl(listOf(getBeerDto()), 1, 25, 1)
+
+    private fun getBeerDto(): BeerDTO =
+        BeerDTO(
+            price = BigDecimal("10.99"),
+            name = "Mango Bobs",
+            style = BeerStyle.IPA,
+            quantity = 500,
+            upc = "12345"
+        )
 }
