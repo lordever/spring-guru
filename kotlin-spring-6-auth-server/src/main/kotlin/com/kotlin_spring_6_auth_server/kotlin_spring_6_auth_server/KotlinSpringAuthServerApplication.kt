1affe7f8a321bd799aff93dd73c2c298a7736adb
lordever <lordever@mail.ru>
2024-09-07 22:50:24 +0500

    Lesson 19.0

---
+++ kotlin-spring-6-auth-server/src/main/kotlin/com/kotlin_spring_6_auth_server/kotlin_spring_6_auth_server/KotlinSpringAuthServerApplication.kt
@@ -0,0 +1,11 @@
+package com.kotlin_spring_6_auth_server.kotlin_spring_6_auth_server
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotlinSpringAuthServerApplication
+
+fun main(args: Array<String>) {
+    runApplication<KotlinSpringAuthServerApplication>(*args)
+}
---
+++ kotlin-spring-6-auth-server/src/test/kotlin/com/kotlin_spring_6_auth_server/kotlin_spring_6_auth_server/KotlinSpringAuthServerApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.kotlin_spring_6_auth_server.kotlin_spring_6_auth_server
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinSpringAuthServerApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
