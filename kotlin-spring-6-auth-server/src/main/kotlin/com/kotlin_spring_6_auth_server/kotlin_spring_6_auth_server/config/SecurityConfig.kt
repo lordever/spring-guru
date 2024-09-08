55b09a25349963298a81d47fcff40daaabeb2e14
lordever <lordever@mail.ru>
2024-09-08 19:27:21 +0500

    Lesson 19.1

---
+++ kotlin-spring-6-auth-server/src/main/kotlin/com/kotlin_spring_6_auth_server/kotlin_spring_6_auth_server/config/SecurityConfig.kt
@@ -0,0 +1,48 @@
+package com.kotlin_spring_6_auth_server.kotlin_spring_6_auth_server.config
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.core.annotation.Order
+import org.springframework.http.MediaType
+import org.springframework.security.config.Customizer
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.config.annotation.web.configurers.ExceptionHandlingConfigurer
+import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer
+import org.springframework.security.oauth2.server.authorization.config.annotation.web.configuration.OAuth2AuthorizationServerConfiguration
+import org.springframework.security.oauth2.server.authorization.config.annotation.web.configurers.OAuth2AuthorizationServerConfigurer
+import org.springframework.security.web.SecurityFilterChain
+import org.springframework.security.web.authentication.LoginUrlAuthenticationEntryPoint
+import org.springframework.security.web.util.matcher.MediaTypeRequestMatcher
+import java.util.*
+
+
+@Configuration
+@EnableWebSecurity
+class SecurityConfig {
+    @Bean
+    @Order(1)
+    @Throws(Exception::class)
+    fun authorizationServerSecurityFilterChain(http: HttpSecurity): SecurityFilterChain {
+        OAuth2AuthorizationServerConfiguration.applyDefaultSecurity(http)
+        http.getConfigurer(OAuth2AuthorizationServerConfigurer::class.java)
+            .oidc(Customizer.withDefaults()) // Enable OpenID Connect 1.0
+
+        // Redirect to the login page when not authenticated from the
+        // authorization endpoint
+        http
+            .exceptionHandling { exceptions: ExceptionHandlingConfigurer<HttpSecurity?> ->
+                exceptions
+                    .defaultAuthenticationEntryPointFor(
+                        LoginUrlAuthenticationEntryPoint("/login"),
+                        MediaTypeRequestMatcher(MediaType.TEXT_HTML)
+                    )
+            } // Accept access tokens for User Info and/or Client Registration
+            .oauth2ResourceServer { resourceServer: OAuth2ResourceServerConfigurer<HttpSecurity?> ->
+                resourceServer
+                    .jwt(Customizer.withDefaults())
+            }
+
+        return http.build()
+    }
+}
