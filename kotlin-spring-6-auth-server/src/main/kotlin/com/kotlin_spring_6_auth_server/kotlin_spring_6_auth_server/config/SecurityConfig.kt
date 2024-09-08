00dfcb35a6d8eac2f6e7b38e94e556757cc0407c
lordever <lordever@mail.ru>
2024-09-08 19:37:25 +0500

    Lesson 19.3

--- kotlin-spring-6-auth-server/src/main/kotlin/com/kotlin_spring_6_auth_server/kotlin_spring_6_auth_server/config/SecurityConfig.kt
+++ kotlin-spring-6-auth-server/src/main/kotlin/com/kotlin_spring_6_auth_server/kotlin_spring_6_auth_server/config/SecurityConfig.kt
@@ -7,11 +7,14 @@ import org.springframework.http.MediaType
 import org.springframework.security.config.Customizer
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
-import org.springframework.security.config.annotation.web.configurers.AuthorizeHttpRequestsConfigurer.AuthorizationManagerRequestMatcherRegistry
 import org.springframework.security.config.annotation.web.configurers.ExceptionHandlingConfigurer
 import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer
+import org.springframework.security.core.userdetails.User
+import org.springframework.security.core.userdetails.UserDetails
+import org.springframework.security.core.userdetails.UserDetailsService
 import org.springframework.security.oauth2.server.authorization.config.annotation.web.configuration.OAuth2AuthorizationServerConfiguration
 import org.springframework.security.oauth2.server.authorization.config.annotation.web.configurers.OAuth2AuthorizationServerConfigurer
+import org.springframework.security.provisioning.InMemoryUserDetailsManager
 import org.springframework.security.web.SecurityFilterChain
 import org.springframework.security.web.authentication.LoginUrlAuthenticationEntryPoint
 import org.springframework.security.web.util.matcher.MediaTypeRequestMatcher
@@ -60,5 +63,16 @@ class SecurityConfig {
             .formLogin(Customizer.withDefaults())
 
         return http.build()
+    }
+
+    @Bean
+    fun userDetailsService(): UserDetailsService {
+        val userDetails: UserDetails = User.withDefaultPasswordEncoder()
+            .username("user")
+            .password("password")
+            .roles("USER")
+            .build()
+
+        return InMemoryUserDetailsManager(userDetails)
     }
 }
