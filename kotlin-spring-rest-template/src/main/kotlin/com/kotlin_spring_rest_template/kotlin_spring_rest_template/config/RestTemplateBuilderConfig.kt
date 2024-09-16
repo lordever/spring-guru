8d7529ff020cc432130e9b9dd29e17a565542b41
lordever <lordever@mail.ru>
2024-09-17 00:17:06 +0500

    Lesson 21.1

--- kotlin-spring-rest-template/src/main/kotlin/com/kotlin_spring_rest_template/kotlin_spring_rest_template/config/RestTemplateBuilderConfig.kt
+++ kotlin-spring-rest-template/src/main/kotlin/com/kotlin_spring_rest_template/kotlin_spring_rest_template/config/RestTemplateBuilderConfig.kt
@@ -5,13 +5,38 @@ import org.springframework.boot.autoconfigure.web.client.RestTemplateBuilderConf
 import org.springframework.boot.web.client.RestTemplateBuilder
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.security.oauth2.client.AuthorizedClientServiceOAuth2AuthorizedClientManager
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientManager
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientProvider
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientProviderBuilder
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientService
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository
 import org.springframework.web.util.DefaultUriBuilderFactory
 
 @Configuration
-class RestTemplateBuilderConfig {
+class RestTemplateBuilderConfig(
+    val clientRegistrationRepository: ClientRegistrationRepository,
+    val oauth2AuthorizedClientService: OAuth2AuthorizedClientService
+) {
 
     @Value("\${rest.template.rootUrl}")
     lateinit var rootUrl: String
+
+    @Bean
+    fun oauth2AuthorizedClientManager(): OAuth2AuthorizedClientManager {
+        val authorizedClientProvider = OAuth2AuthorizedClientProviderBuilder.builder()
+            .clientCredentials()
+            .build()
+
+        val oauth2AuthorizedClientManager = AuthorizedClientServiceOAuth2AuthorizedClientManager(
+            clientRegistrationRepository,
+            oauth2AuthorizedClientService
+        )
+
+        oauth2AuthorizedClientManager.setAuthorizedClientProvider(authorizedClientProvider)
+
+        return oauth2AuthorizedClientManager
+    }
 
     @Bean
     fun restTemplateBuilder(configurer: RestTemplateBuilderConfigurer): RestTemplateBuilder =
