4c2d228ae568da493e4bffe46e4295ea3959476c
lordever <lordever@mail.ru>
2024-10-06 13:19:09 +0500

    Lesson 23.3

---
+++ kotlin-spring-r2dbc/src/main/kotlin/com/kotlin_spring_r2dbc/kotlin_spring_r2dbc/domain/Beer.kt
@@ -0,0 +1,17 @@
+package com.kotlin_spring_r2dbc.kotlin_spring_r2dbc.domain
+
+import java.math.BigDecimal
+import java.time.LocalDateTime
+import org.springframework.data.annotation.Id
+
+data class Beer(
+    @Id
+    val id: Int,
+    val name: String,
+    val style: String,
+    val upc: String,
+    val quantity: Int,
+    val price: BigDecimal,
+    val createdDate: LocalDateTime,
+    val lastModifiedDate: LocalDateTime,
+)
