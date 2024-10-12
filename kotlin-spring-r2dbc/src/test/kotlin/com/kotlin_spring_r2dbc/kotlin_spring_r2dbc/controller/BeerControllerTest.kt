ac5cee9b3c69e22a9e6db6d06a82b72315c8190a
lordever <lordever@mail.ru>
2024-10-12 18:58:17 +0500

    Lesson 25.1

---
+++ kotlin-spring-r2dbc/src/test/kotlin/com/kotlin_spring_r2dbc/kotlin_spring_r2dbc/controller/BeerControllerTest.kt
@@ -0,0 +1,25 @@
+package com.kotlin_spring_r2dbc.kotlin_spring_r2dbc.controller
+
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.web.reactive.server.WebTestClient
+
+@SpringBootTest
+@AutoConfigureWebTestClient
+class BeerControllerTest {
+    @Autowired
+    lateinit var webTestClient: WebTestClient
+
+    @Test
+    fun testListBeers() {
+        webTestClient
+            .get()
+            .uri(BeerController.BEER_PATH)
+            .exchange()
+            .expectStatus().isOk
+            .expectHeader().valueEquals("Content-type", "application/json")
+            .expectBody().jsonPath("$.size()").isEqualTo(3)
+    }
+}
